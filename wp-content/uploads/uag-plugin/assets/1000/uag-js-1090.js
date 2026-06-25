8b393a7c670a3c577b0aa662ad58f6169afa941d
bincentmcr <bincentmcr@gmail.com>
2026-06-25 11:06:31 +0800

    Add Istorya by Eric portfolio showcase

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1090.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-ce080147' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-c3e8b55e' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-f2b9ac34' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-22414180' );
+});
+ });
